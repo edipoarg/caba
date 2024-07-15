04dc28b9cae31561834acaddd3d2076883414205
anon author <noemail@nothing.null>
2024-07-15 14:29:32 -0300

    Updates

---
+++ data/index.js
@@ -0,0 +1,12 @@
+import departamentos from "../data/departamentos.json";
+import caba from "../data/caba.json";
+import barriosCaba from "../data/barriosCaba.json";
+import departamentosLaPlata from "../data/departamentosLaPlata.json";
+import laPlata from "../data/laPlata.json";
+import dependenciasCaba from "../data/dependenciasCaba.json"
+import  gatillo  from "../data/gatilloCaba.json";
+import  reportes  from "../data/reportesCaba.json";
+
+
+
+export {  departamentos, caba, barriosCaba, laPlata, departamentosLaPlata, gatillo, reportes, dependenciasCaba };
