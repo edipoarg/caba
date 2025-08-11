e2e5adee8ee62d5b163c9f65db62126e0611f9b9
solsticiodeverano <talleresfinge@gmail.com>
2025-08-11 16:30:28 -0300

    eslint delete white spaces

--- src/components/Investigaciones/todasInvest/Nota16.jsx
+++ src/components/Investigaciones/todasInvest/Nota16.jsx
@@ -7,29 +7,22 @@ const Nota16 = () => {
                 <h4 className={styles.singularText}>
                     En enero de 2025, la Policía de la Ciudad se instaló oficialmente en el Bajo Flores. Durante más de 15 años, la fuerza que ocupó ese lugar fue Gendarmería Nacional (GNA). El sur de la ciudad de Buenos Aires, además de ser intencionalmente marginado por los sucesivos gobiernos, respira una dinámica propia en materia de seguridad. Este territorio sin independencia tiene un perímetro difuso que se dibuja constantemente por la vida de sus habitantes: el vínculo de los vecinos con los agentes, el rol del narcotráfico y el abordaje de un plan formulado y ejecutado sin conocer las necesidades de quienes habitan el territorio.
                 </h4>
-
                 <h3 className={styles.subTitle}>Un paisaje: ¿Flores o Bajo?</h3>
-
                 <h4 className={styles.singularText}>
                     Establecer los límites del Bajo Flores dentro del mapa es difícil, pero no imposible: se trata de un barrio no oficial, con contornos indefinidos, en el extremo sur de Flores y en el límite con Nueva Pompeya. Su principal punto de referencia es el estadio Nuevo Gasómetro, del Club San Lorenzo de Almagro.
                 </h4>
-
                 <h4 className={styles.singularText}>
-                    El acceso más evidente se advierte al avanzar hacia el sur por avenida Varela, desde su intersección con Eva Perón. Si se pasa por el Hospital Piñero, uno de los principales centros de salud al que acceden los vecinos, se llega al Cementerio, entre florerías, marmolerías y broncerías –negocios típicos de las inmediaciones de todo camposanto citadino–. Allí también se encuentra la terminal del colectivo 132 y pasa el 76, único medio de transporte que se interna en la villa de manera sostenida.
+                    El acceso más evidente se advierte al avanzar hacia el sur por avenida Varela, desde su intersección con Eva Perón. Si se pasa por el Hospital Piñero, uno de los principales centros de salud al que acceden los vecinos, se llega al Cementerio, entre florerías, marmolerías y broncerías -negocios típicos de las inmediaciones de todo camposanto citadino-. Allí también se encuentra la terminal del colectivo 132 y pasa el 76, único medio de transporte que se interna en la villa de manera sostenida.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     En abril de este año, la línea 23 acortó su recorrido y dejó de ingresar al Bajo, limitando aún más las opciones de traslado de los vecinos. Solo el reclamo y la presión comunitaria logró que se revirtiera esta decisión hace unas semanas.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Más adelante está la esquina de Varela y Castañares. Los cuatro semáforos que permiten giros en ambas direcciones resultan ajenos a toda lógica peatonal: sus luces desincronizadas reflejan a un Estado que tampoco se entiende con el territorio. En consecuencia, e incluso cuando están en funcionamiento que es la mitad de las veces, no se sabe cuándo es seguro cruzar.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Es una tarea colectiva que marca la táctica de supervivencia: alguien atina a tomar la posta y los demás se suman. Los autos y camiones frenan de prepo y se entra a esa zona que, por acuerdo implícito, corresponde al Bajo Flores.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     El reconocimiento de este límite no es azaroso, porque lo importante es entender que a partir de acá, las lógicas de la seguridad y las leyes no son las mismas que para el resto de la ciudad. Y no es una sensación infundada, tiene ya varios años de historia.
                 </h4>
@@ -37,7 +30,6 @@ const Nota16 = () => {
                     El Operativo Cinturón Sur estaba dirigido a los barrios de Bajo Flores, Nueva Pompeya, Lugano, Soldati, Barracas y La Boca.
                 </h6>
                 <h3 className={styles.subTitle}>Tradición y conflictos: ¿a quién le corresponde?</h3>
-
                 <h4 className={styles.singularText}>
                     En 2008, el comienzo de la{" "}
                     <a
@@ -55,7 +47,6 @@ const Nota16 = () => {
                     </a>
                     ) dirigido a los barrios de Bajo Flores, Nueva Pompeya, Lugano, Soldati, Barracas y La Boca. Según reza el texto publicado entonces, su objetivo era “optimizar el servicio de seguridad ciudadana en la Capital Federal mediante la complementación operativa sinérgica de los cuerpos policiales y de las fuerzas de seguridad federales por zonas”.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Luego, en 2017 con la renombrada Policía de la Ciudad, el operativo fue dado de baja con el decreto{" "}
                     <a
@@ -66,69 +57,52 @@ const Nota16 = () => {
                     </a>{" "}
                     por el entonces presidente Mauricio Macri. Sin embargo, en la práctica, las fuerzas federales siguieron operando. La Policía de la Ciudad se limitaba a accionar solamente en las orillas.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Así lo relata S., una militante social con años de recorrida del barrio que ve cómo el límite se marca por la fuerza: “Agarro por Castañares y veo dos situaciones horribles. En la primera, dos policías que estaban en moto cruzan de una forma súper violenta y se suben a la vereda. Venían dos pibitos con cara de nenes que estaban comiendo. Les gritaron, les cruzaron la moto y los pusieron contra la pared para palparlos. Tenían 12, 13 años, como mucho. Incluso se notaba que venían de la escuela porque estaban con la mochila.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Hago un tramito más y ahí mismo por Castañares se usa mucho que vayan a quemar los cables para sacarles el cobre. Unas personas estaban en eso y llegó una camioneta de la Policía, se les puso adelante y los increparon también de manera súper violenta”.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Según relatan algunos vecinos, la relación con esa fuerza siempre fue de miedo y desconfianza. La GNA, en cambio, parecía tener una presencia más cercana de identificación con el territorio y sus habitantes. En definitiva, estaban adentro. Y cuando se trabaja tantos años en delimitar un adentro y un afuera, esa integración tiene significados complejos.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     “La Policía de la Ciudad no entra para darte un certificado de domicilio, pero para una requisa o una denuncia sí. Traen citaciones o denuncias del juzgado, hacen razzias o requisas, pero no traen seguridad. Gendarmería trae seguridad”, cuenta S.
                 </h4>
-
                 <h6 className={styles.span}>
                     En 2017, con la renombrada Policía de la Ciudad, el operativo fue dado de baja, pero en la práctica, la Gendarmería y el resto de fuerzas federales siguieron operando.
                 </h6>
-
                 <h3 className={styles.subTitle}>La libertad de los orilleros y la sociedad de los otros</h3>
-
                 <h4 className={styles.singularText}>
                     En el Bajo viven argentinos y migrantes peruanos, paraguayos y bolivianos. No habitan los mismos espacios, las comunidades se establecen en diferentes sectores dentro del Bajo: Rivadavia, Illia y Ricciardelli o Villa 1-11-14. El centro comercial o área de mayor movimiento económico y trabajo de los habitantes está situado justo en el medio, por la Avenida Riestra.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Pablo Fernández Rojas, director del canal de YouTube Causa Parcial, nació en Bolivia y creció en el barrio a partir de su primer año. En este caso, comparte su lectura de la dinámica en materia de seguridad en este territorio antes de la llegada de Gendarmería: “La violencia era en el Barrio Rivadavia y el Barrio Illia. Se afanaba a la gente que salía de laburar de la villa. Es decir, se ejercía violencia hacia adentro”.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Y, en ese sentido, tiene una interpretación más profunda sobre esta situación: “Los pibes que son de ascendencia argentina y que creen que tienen el derecho de la natividad eligen afanar a los extranjeros por eso: porque saben que hay menos derechos ahí para ellos”. En la interseccionalidad de la marginación, el conflicto interno brotaba constantemente.
                 </h4>
-
                 <h4 className={styles.singularText}>
-                    Con la llegada de GNA y la instalación de puestos o “garitas” dentro del bajo y principalmente en la villa 1-11-14, esto mermó: “Solucionaron un problema y por eso se los valoró tanto. Lo que pasa con la Gendarmería –y para mí es clave entenderlo– es que son agentes externos igual que la gente que vive en la villa.
+                    Con la llegada de GNA y la instalación de puestos o “garitas” dentro del bajo y principalmente en la villa 1-11-14, esto mermó: “Solucionaron un problema y por eso se los valoró tanto. Lo que pasa con la Gendarmería -y para mí es clave entenderlo- es que son agentes externos igual que la gente que vive en la villa.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     O sea, te traen un pibe de Misiones, por ejemplo. Se integran bien al barrio porque tienen los mismos problemas del barrio, porque comen lo mismo que nosotros, viven lo mismo que nosotros, están en la noche, viven nuestra vida y tienen los problemas que tendrían en el barrio. Entonces, la integración de ellos real es esa. Ya eran extranjeros antes de venir, ya eran agentes que no tenían que ver con la Policía de la Ciudad, que a lo sumo viene del Conurbano. Los gendarmes tampoco son de acá.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Solamente representan una posición estatal pero, ¿qué tan distinto es un paraguayo de un gendarme?” reflexiona Pablo.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Por otro lado, los oficiales de la fuerza local parecen funcionar como una aduana de hecho. Es decir, controlan el ingreso y egreso de los habitantes del Bajo. Actúan en las orillas como un dispositivo de regulación fronterizo en medio de lo que Pablo describe como corrupción a cielo abierto: “Antes, la Policía entraba solamente a allanar y también entraba la Brigada, que era la policía de civil.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Cuando pusieron a Gendarmería adentro del barrio, la Policía solamente iba hasta Varela y Castañares, los límites. Y así se convirtió en un agente de aduanas. Su única relación y negocio con el barrio era frenar a las camionetas de los talleres textiles, sacarles la mercadería y reventarlos, saber a quién y cómo parar. Cuidar hacia la ciudad lo que sale, guetificarlo”.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Esta acción en los márgenes parece limitarse a una labor orientada a desgranar el flujo económico del Bajo: quién produce qué, quién tiene y qué tipo de talleres, cómo se manejan las mercancías, quiénes son los actores involucrados y cómo intervenir para sacar provecho mediante el control. Un agente que, históricamente y a través de la ostentación de poder, teje en silencios y apropiaciones.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Dicta quién puede avanzar y quién debe quedarse en los márgenes. Quizás un indicio -no menor- del propio arquetipo que cosechó la Policía de la Ciudad en la villa.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     De todos modos, el abuso de poder por parte de la Gendarmería también fue problemático. Al recordar ese período, se mencionan oficiales que invitaban a pelear “de puño” a pibes, a veces, menores de edad; casos de gatillo fácil como el de{" "}
                     <a
@@ -146,7 +120,6 @@ const Nota16 = () => {
                     </a>
                     ” –hacer saltos de rana– por circular sin barbijo durante la cuarentena.
                 </h4>
-
                 <h4 className={styles.singularText}>
                     Estos hechos evidencian la complejidad tanto de la actividad vecinal como la de los que llegan como trabajadores “del orden” y encuentran un territorio liberado para sus excesos. Pero para estar más adentro, para conocer realmente las necesidades de un espacio hay que estar, ir hacia el centro. Y en el centro de la villa, ahí es donde está el narco.
                 </h4>
