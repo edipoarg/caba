82d96108537f1bbcba565e10753d387dbc64af8b
solsticiodeverano <talleresfinge@gmail.com>
2026-03-19 14:54:16 -0300

    nota 23

---
+++ src/components/Investigaciones/todasInvest/Nota23.jsx
@@ -0,0 +1,131 @@
+import styles from "./TodasInvestigaciones.module.css";
+
+const Nota23 = () => {
+  return (
+    <>
+      <div className={styles.NotaContainer}>
+        <h4 className={styles.singularText}>
+          La doctrina de seguridad de corte punitivista instaurada por la exministra de Seguridad, Patricia Bullrich, y su heredera política, Alejandra Monteoliva, es el modelo copiado por Jorge Macri en la Ciudad de Buenos Aires (CABA), lo que implica cada vez más personas detenidas. De acuerdo al Observatorio de Políticas Penitenciarias y Derechos Humanos del Consejo de la Magistratura de CABA, la cantidad de personas privadas de la libertad en comisarías y alcaidías de la Ciudad aumentó un  148%, entre diciembre del 2022 (961) y febrero de 2026 (2384).
+        </h4>
+        <h4 className={styles.singularText}>
+          Sin embargo, este sistema pende de un hilo. Hacinamiento, motines, falta de higiene y otros problemas le restan credibilidad a la idea de que el paraíso de represión de este gobierno sea posible. El Mapa de la Policía llevó adelante visitas regulares a distintos establecimientos, registró y entrevistó a los internos.
+
+        </h4>
+        <h3 className={styles.subTitle}>los datos</h3>
+        <h4 className={styles.singularText}>A marzo de 2026, la Policía de la Ciudad cuenta con tres tipos de lugares de encierro:
+        </h4>
+        <h4 className={styles.singularText}>. 52 alcaidías
+        </h4>
+        <h4 className={styles.singularText}>. 9 comisarías vecinales
+        </h4>
+        <h4 className={styles.singularText}>. 7 espacios transitorios que funcionan dentro de las comisarías vecinales
+        </h4>
+        <h4 className={styles.singularText}>Las comisarías y alcaidías en la Ciudad tienen la función de gestionar las detenciones en las primeras horas por delitos in fraganti o por orden judicial y la permanencia en estos espacios debería ser breve. Es decir, no más de 72 horas. El acondicionamiento, entonces, debería estar en consonancia con la normativa y responder a esta lógica de transitoriedad. Sin embargo, ahí es donde se abre una brecha: hay quienes llevan más de un año y medio de detención y también condenados para quienes no llega nunca el traslado a las penitenciarías.
+        </h4>
+        <h4 className={styles.singularText}>Las consecuencias relevadas por el Mapa de la Policía son elocuentes:
+        </h4>
+        <h4 className={styles.singularText}>. No hay camas ni camastros suficientes. En algunos casos la única superficie disponible para el descanso es una gomaespuma colocada directamente sobre el piso en la que se duerme y se come. En muchos casos deben turnarse para dormir.
+        </h4>
+        <h4 className={styles.singularText}>. Las condiciones de higiene son insuficientes. </h4>
+        <h4 className={styles.singularText}>. La mayoría de los espacios carece de luz natural, por lo que las luces artificiales permanecen encendidas durante todo el día.
+        </h4>
+        <h4 className={styles.singularText}>. Las celdas de las alcaidías cuentan con letrinas destinadas únicamente a deposiciones líquidas y, para otras necesidades, las personas detenidas deben solicitar salir a baños comunes -traslados que  realizan esposadas-.
+        </h4>
+        <h4 className={styles.singularText}>Además, se constató lo señalado en el Informe Anual 2024 del Mecanismo Local de Prevención de la Tortura de CABA que indica que el sistema de salud “se encuentra obstruido por un circuito burocrático ineficiente que naturaliza la dilación y niega el carácter urgente de la atención médica”. </h4>
+        <h4 className={styles.singularText}>Más allá de lo mencionado, las entrevistas con los internos permiten resaltar una demanda insistente en la necesidad de aire, de respirar. Muchas de las celdas ubicadas en las comisarías no fueron diseñadas para alojar personas, sino que son oficinas mal adaptadas con el agregado de rejas y puertas de hierro aseguradas. En la mayoría de los casos no cuentan con ventanas, por lo que el calor y la humedad se acumulan y vuelven el aire demasiado pesado.
+        </h4>
+        <h4 className={styles.singularText}>El segundo requerimiento que aparece con frecuencia es la dificultad para recibir visitas. Los lugares de esparcimiento son prácticamente inexistentes y el acceso a los allegados suele verse limitado por la falta de espacio físico disponible. Estos aspectos, que desde el punto de vista humano carecen de consideración, no parecen ser casuales. La Alcaidía 3C que el Mapa de la Policía visitó en enero de 2026, y cuyo edificio fue inaugurado por Horacio Rodríguez Larreta en 2023, tiene los mismos problemas. Esto habla no solo de una precariedad sistemática, sino también de una decisión que se sostuvo a pesar de las experiencias recogidas en establecimientos más antiguos.</h4>
+        <h4 className={styles.singularText}>Por último, la revisión de las actas junto con las entrevistas hechas por el equipo de visitas dio cuenta de que la mayoría de las personas detenidas están en situación de vulnerabilidad, con lo que su detención completaría la política del gobierno de la Ciudad de Buenos Aires de “limpiar” la ciudad. No de basura -una queja real y no resuelta-, sino de quienes la habitaban: personas en situación de calle, trabajadoras sexuales o con consumo problemático. </h4>
+
+        <h6 className={styles.span}>
+          Entre la norma y la práctica se abre una brecha: hay personas que llevan más de un año y medio de detención en comisarías y personas condenadas para quienes no llega nunca el traslado a las penitenciarías.
+        </h6>
+
+        <h3 className={styles.subTitle}>el derrame del colapso</h3>
+
+        <h4 className={styles.singularText}>Para  entender el problema estructural de las comisarías y alcaidías llenas, también es necesario poner el foco en los penales llenos, las demoras en los traslados de condenados, las numerosas detenciones y la dificultad para acceder a mecanismos de salida.
+        </h4>
+
+        <h4 className={styles.singularText}>La  <a href="https://servicios.infoleg.gob.ar/infolegInternet/anexos/35000-39999/37872/texact.htm" className={styles.link} target="_blank" rel="noopener noreferrer">
+          Ley 24.660
+        </a>  de Ejecución Penal sufrió dos modificaciones en ese sentido  que están reflejadas en el artículo 56 y 56 bis del texto, donde se enumeran 11 ítems que contienen delitos excluidos de la posibilidad de tener libertad condicional, prisión discontinua o semidetención y la libertad asistida. El primer cambio vino en el paquete de las mediáticamente llamadas “Leyes Blumberg” - particularmente la  <a href="https://servicios.infoleg.gob.ar/infolegInternet/anexos/100000-104999/100947/norma.htm" className={styles.link} target="_blank" rel="noopener noreferrer">
+            Ley 25.948
+          </a> - y  luego se sumó otro grupo en 2017 con la sanción de la  <a href="https://servicios.infoleg.gob.ar/infolegInternet/anexos/275000-279999/277449/norma.htm" className={styles.link} target="_blank" rel="noopener noreferrer">
+            Ley 27.375
+          </a> . Por otra parte, también se modificó el Código Penal en su artículo 14, estableciéndose  las mismas especificaciones con aplicación para todo el país.
+        </h4>
+
+        <h4 className={styles.singularText}>El dato es relevante para pensar  cómo estas modificaciones aportan al colapso de los penales que -en términos habitacionales- alojan más de lo que pueden recibir y liberan menos de lo que necesitan.
+        </h4>
+
+        <h4 className={styles.singularText}>Por otro lado, la Ciudad no tiene Sistema Penitenciario propio. Por lo tanto las acciones en este ámbito están regidas por un convenio  que establece que a los presos de la Ciudad les corresponde ir a unidades penitenciarias federales del Área Metropolitana de Buenos Aires. Esto traza un puente directo entre los problemas que se dan a nivel federal y los de CABA. Según el   <a href="http://argentina.gob.ar/sites/default/files/2025/09/informe_sneep_argentina_2024_6.pdf" className={styles.link} target="_blank" rel="noopener noreferrer">
+          último informe
+        </a>  (2024) del Sistema Nacional de Estadísticas sobre Ejecución de la Pena (SNEEP), en los últimos 20 años se pasó de 54.472 a 120.700 personas privadas de libertad en todo el país.
+        </h4>
+        <br />
+
+        <img
+          src="https://static.wixstatic.com/media/0f4ca0_35b1b8c3d88046d286698888d56364a7~mv2.jpg/v1/fill/w_739,h_585,al_c,q_85,enc_avif,quality_auto/mdp.jpg"
+          alt="Evolución de población penitenciaria"
+          className={styles.image3}
+        />
+
+
+        <h6 className={styles.span}>
+
+          La ventilación en estos espacios es insuficiente o directamente inexistente. En la mayoría de los casos no cuentan con ventanas. El calor y la humedad se acumulan y vuelven el aire demasiado pesado.
+        </h6>
+
+        <h3 className={styles.subTitle}>la vía institucional</h3>
+
+
+        <h4 className={styles.singularText}>Siguiendo esta línea argumental de que el problema viene de arriba, podemos rastrear un llamado de atención de hace casi seis años. En marzo de 2020, y a causa de las medidas de distanciamiento social que se estaban tomando en ese momento por la propagación del virus COVID-19, el Servicio Penitenciario Federal impartió directivas respecto a la restricción de admisión de internos e ingresos de personas a los establecimientos penitenciarios. Es decir, para evitar una sobrepoblación que volviera imposible el distanciamiento entre internos, empezaron a recibir muchos menos.
+        </h4>
+
+        <h4 className={styles.singularText}>Un hábeas corpus presentado por el Ministerio Público de la Defensa del 20 de junio de ese año expuso que “la adopción esa medida tuvo la consecuencia práctica que las personas respecto de las cuales se dictó prisión preventiva o condena desde el 20/03/20 en adelante y en el ámbito territorial de la CABA fueron ingresadas en el Centro de Alojamiento de Contraventores y en las Alcaidías a cargo de la Policía de la Ciudad de Buenos Aires a la espera de ser trasladadas a los penales correspondientes”. Incluso se mencionaba que “la sanción de la ley 27.375 [la de 2017] coadyuvó al acrecentamiento del problema”. Y este escenario representaba un grave problema, porque “esta decisión tiene consecuencias preocupantes respecto de las personas allí detenidas ya que estos lugares no cumplen con los estándares de derechos humanos observables”.
+        </h4>
+
+        <h4 className={styles.singularText}>Según pudo averiguar el <strong> Mapa de la Policía</strong>, en diciembre de 2025 la situación no solo no había mejorado, sino que había empeorado. Así lo refleja la resolución del Juzgado de Primera Instancia en lo penal, contravencional y de faltas Nº3 a cargo de la jueza María Mercedes Maiorano. Allí se intima por octava vez desde la presentación del hábeas corpus al Subsecretario de Asuntos Penitenciarios Julián Marcelo Curi y al Director Nacional del Servicio Penitenciario Federal, Femando Martínez, a que se dé inicio al proceso de traslado de todas las personas detenidas en alcaidías y comisarías de la Policía de la Ciudad condenadas o con la situación procesal resuelta y se les otorgue una unidad de destino. Además, se le indica al jefe de Policía de CABA, Diego Ariel Casaló, abstenerse de alojar en las comisarías y alcaidías bajo su custodia a personas privadas de la libertad, condenadas o con la situación procesal resuelta.
+        </h4>
+
+        <h4 className={styles.singularText}>En ese sentido, la argumentación de la jueza Maiorano señala que “en la actualidad, ha sido dejada atrás la problemática que originó el cierre de ingresos al sistema penitenciario federal, esto es, la pandemia. Sin embargo, con el fin de la situación sanitaria, no se revirtieron las políticas que derivaron en la reducción del otorgamiento de plazas, sino que por el contrario, se insistió con la metodología adoptada desde entonces. Con ello, tampoco cesó la situación de alojamiento transitorio que debió afrontarse para superar las dificultades que acompañaron al COVID-19”.
+        </h4>
+        <h4 className={styles.singularText}>Además de esta evidencia, las visitas llevadas a cabo por este colectivo dan cuenta de que las órdenes por parte del Poder Judicial no están siendo cumplidas y no asoma otro horizonte posible. La vía institucional, por lo tanto, parece estancada y el repaso de lo que pasó en estos años no hace más que evidenciar un pantano de resoluciones que, sin conexión con lo que efectivamente sucede en los espacios de encierro, quedan archivadas en el correo basura de los responsables.
+        </h4>
+
+        <h6 className={styles.span}>
+          Para  entender el problema estructural de las comisarías y alcaidías llenas, es necesario poner el foco en los penales llenos, las demoras en los traslados de condenados, las numerosas detenciones y la dificultad para acceder a mecanismos de salida.
+        </h6>
+        <h3 className={styles.subTitle}>la guerra</h3>
+        <h4 className={styles.singularText}>Una metáfora no es simplemente un adorno en el discurso, es una forma del pensamiento. La metáfora crea una tensión: permite ver algo como otra cosa sin que deje de ser lo que es. Básicamente una metáfora  produce desplazamientos. Cada vez que trasladamos un esquema de pensamiento a otro ámbito que no le es propio, reorganizamos lo que entendemos.
+        </h4>
+
+        <h4 className={styles.singularText}>Una metáfora bélica toma el vocabulario y la estructura de la guerra —enemigo, ofensiva, victoria, territorio— y los proyecta sobre fenómenos que no son guerras. Cuando la seguridad pública se formula como combate y los delincuentes son narrados como la amenaza a derrotar, se establece el marco en el que  pensamos el problema. En los discursos en los que se usa la metáfora de la guerra para hablar de seguridad, la sociedad aparece dividida en bandos.
+        </h4>
+
+        <h4 className={styles.singularText}>Desde el discurso de las autoridades del Gobierno se está construyendo una metáfora bélica que no señala a la delincuencia como el enemigo a derrotar, sino a los delincuentes. Es importante este corrimiento, que puede parecer sutil pero cuyas implicaciones no son menores. Porque el objetivo entonces no parece ser prevenir el delito sino demonizar a los sujetos que lo cometen. Vemos, por ejemplo, cómo se nomina "terroristas" o "violentos" a los manifestantes o cómo las personas en situación de calle son detenidas por "resistencia a la autoridad".
+        </h4>
+
+        <h4 className={styles.singularText}>  <a href="https://www.youtube.com/watch?v=ba0M9sK4dqc" className={styles.link} target="_blank" rel="noopener noreferrer">
+          Durante los debates presidenciales que se transmitían por televisión pública en 2023          </a>,  el entonces candidato a presidente Javier Milei hablaba sobre sus propuestas para la seguridad nacional y afirmaba: “En una Argentina liberal, los que la van a pasar mal son los delincuentes”. Milei convierte el sufrimiento de los delincuentes en promesa política. Prometer que alguien “la va a pasar mal” significa ofrecer el padecimiento como horizonte deseable. Significa que el castigo es una expectativa que pretende ser celebrada por la sociedad. En una campaña electoral la promesa de sufrimiento  organizaba la escena política en torno a la idea de que el dolor de ciertas personas es parte del orden que se quiere construir.
+        </h4>
+
+        <h4 className={styles.singularText}>En otra intervención institucional sostuvo: “Nuestra doctrina de seguridad: los buenos son los de azul, los malos son los que delinquen” [comunicado oficial de Presidencia, 2024]. Una partición moral absoluta. La metáfora bélica ordena el sentido bajo la lógica de buenos y malos, amigos y enemigos.
+        </h4>
+
+        <h4 className={styles.singularText}>La repetición de estas fórmulas desde la amplificación del poder —discursos presidenciales, comunicados estatales, circulación mediática— fija asociaciones. La metáfora empieza a hacer su trabajo. En la guerra, el enemigo no es interlocutor sino amenaza. En la guerra el enemigo no merece respeto. Si el delito se piensa bajo ese marco, quien delinque deja de ser persona para convertirse en el mal y la vulneración de derechos aparece como tolerable.
+        </h4>
+
+        <h4 className={styles.singularText}>Lo vimos repetirse muchas veces a lo largo de la historia. La deshumanización va antes de cualquier declaración abierta. Comienza con una clasificación que excluye. Durante siglos los esclavos fueron considerados propiedad antes que personas plenas; la pregunta sobre si poseían alma revela hasta qué punto la humanidad puede ponerse en duda cuando un grupo es reducido a función o peligro. La metáfora bélica aplicada a la seguridad no afirma explícitamente que quienes están privados de libertad carezcan de derechos, pero al inscribirlos en el campo del enemigo, facilita que la opinión pública tolere condiciones inhumanas que no aceptaría para quienes reconoce como semejantes.
+        </h4>
+
+        <h6 className={styles.span}>
+
+          Desde el discurso de las autoridades del Gobierno se está construyendo una metáfora bélica que no señala a la delincuencia como el enemigo a derrotar, sino a los delincuentes. </h6>
+
+      </div>
+    </>
+  );
+};
+
+export default Nota23;
